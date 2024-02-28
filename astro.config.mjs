f9349fcac0671b11a9b93efff26e96a09053fde6
Progyan Bhattacharya <bprogyan@gmail.com>
2024-02-28 11:51:04 +0530

    (chore) Remove Vercel Runtime Dependency
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-// import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,7 +24,4 @@ export default defineConfig({
     prefetchAll: true,
     defaultStrategy: 'viewport',
   },
-  // adapter: vercel({
-  //   analytics: true,
-  // }),
 });

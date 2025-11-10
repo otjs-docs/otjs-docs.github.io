7ff82a2df65c8ed88827f9d3ad410e520708eb9e
Progyan Bhattacharya <bprogyan@gmail.com>
2025-11-10 17:47:32 +0530

    feat: Documentation for Editors and OT
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 import react from "@astrojs/react";
+import sharp from "sharp";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
@@ -8,14 +10,18 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://ot.js.org",
   compressHTML: true,
+  image: {
+    service: sharp(),
+  },
   integrations: [
+    icon(),
+    react(),
     mdx({
       syntaxHighlight: "shiki",
       shikiConfig: { theme: 'github-dark-dimmed' },
       gfm: true,
     }),
     sitemap(),
-    react(),
   ],
   prefetch: {
     prefetchAll: true,
@@ -24,6 +30,7 @@ export default defineConfig({
   redirects: {
     "/docs": "/docs/getting-started"
   },
+  trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
   },
